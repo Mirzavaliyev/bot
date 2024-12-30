0c009dca6beb7bdec9a22169df82e93a32d9c144
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-30 11:39:01 +0500

    work

--- bot1.py
+++ bot1.py
@@ -1,10 +1,27 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.loggers import dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
 
-API_TOKEN = 'BOT TOKENI'
+API_TOKEN = '7840981516:AAEqJNWYSMGXQT1sSgOaZHYCOgCyRkKBVG4'  # O'zingizning bot tokeningizni kiriting
+
 logging.basicConfig(level=logging.INFO)
+
+# Bot va Dispatcher obyektlarini yaratish
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-@dp.message_handler(commands=['start','help'])
+storage = MemoryStorage()  # Bot xotirasi
+dp = Dispatcher(bot, storage=storage)
+
+# '/start' va '/help' komandalari uchun handler
+@dp.message(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
+    await message.answer("Salom! Men Telegram botman. Sizga qanday yordam bera olaman?")
+
+# Har qanday xabarni qaytarish uchun handler
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+# Pollingni boshlash
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(dp.start_polling(skip_updates=True))
