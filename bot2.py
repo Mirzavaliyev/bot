197eb4a8cdf64dc87b6a853623a929c383dc3c7e
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-30 23:59:19 +0500

    woe

--- bot2.py
+++ bot2.py
@@ -1,2 +1,16 @@
-
-
+from aiogram Bot, Dispatcher, executor, types
+# Bot tokenini ulash
+TOKEN = 'Bot token'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+# Start kommandasi uchun hander
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply('Salom qaanday yordam kerak')
+# /help komandasi uchun hamdler
+@dp.message_handler(command=['help'])
+async def send_help(message: types.Message)
+    await message.reply('Salom bu qanday yordam bera olaman')
+# Botni ishga tushurish
+if __name__ == '__name__':
+    executor.start_polling(dp,skip_updates=True)
