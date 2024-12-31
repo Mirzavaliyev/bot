8d2e58e579e7a59f127ae9e69575165260b08282
Mirzavaliyev <shavkatziyo98@gmail.com>
2024-12-31 14:25:19 +0500

    work

--- bot2.py
+++ bot2.py
@@ -12,6 +12,16 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 # Menu for handler command
 
+@dp.message(Command(commands=['menu']))
+async def show_menu(message: types.Message):
+    # Creating buttons
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Unit 1', callback_data='unit1')],
+        [InlineKeyboardButton(text='Unit 2', callback_data='unit2')]
+    ])
+    await message.answer('O\'zingizga yoqqan bo\'limni tanlang', reply_markup=markup)
+# Unit 1 uchun handler
+
 
 
 
